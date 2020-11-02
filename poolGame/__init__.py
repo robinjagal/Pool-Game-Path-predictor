ae5db6002377a297e5aba55662ce13febd6bb3c8
Robin <robinjagal31@gmail.com>
2020-11-03 03:09:11 +0530

    Added video-feed.html which enables webcam and takes the frames and puts the data into an image tag's source (dynamic)

--- poolGame/__init__.py
+++ poolGame/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from flask_socketio import SocketIO, emit
 app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 from poolGame import routes
 
